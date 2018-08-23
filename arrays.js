cc3c8c5823d53bf25cfa11930cbf1b31dc6e8f42
John Paul Schmidt <mc.wren@gmail.com>
2018-08-23 16:40:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,6 +21,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-  array = [1, 2, 3];
+  var array = [1, 2, 3];
   console.log(array[2]);
 }
