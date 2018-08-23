786d345731e90fb4026a9cb90742659cc25a1719
John Paul Schmidt <mc.wren@gmail.com>
2018-08-23 16:29:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,16 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  array = ["foo", 1]
+  array = ["foo", 1];
   return array;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift("foo")
+  array.unshift("foo");
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  array = [1, "foo"];
   return array;
 }
