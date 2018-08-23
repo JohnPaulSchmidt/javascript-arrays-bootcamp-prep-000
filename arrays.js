32601c0c56fc090c8b0647d080478efcb9c20417
John Paul Schmidt <mc.wren@gmail.com>
2018-08-23 16:52:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,6 +32,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 
 function removeElementFromBeginningOfArray(array) {
   array = [1, 2, 3]
-  array.slice(0);
+  array.slice(1);
   return array;
 }
