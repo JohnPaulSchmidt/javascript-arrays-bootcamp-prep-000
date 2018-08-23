58d12389b9e5c5ae21ca6b6fabc44be3dcce98a9
John Paul Schmidt <mc.wren@gmail.com>
2018-08-23 13:53:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  var array = [1];
+  const array = [1];
   element = 'foo';
   [element, ...array]
   console.log(array)
